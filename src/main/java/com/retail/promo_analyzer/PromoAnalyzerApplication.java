12f2cc754e246eb16b0b1c2278561b4602388f1e
Muravyev Konstantin <k.muravyev2016@gmail.com>
2025-04-15 01:48:50 +0300

    Initial commit

---
+++ src/main/java/com/retail/promo_analyzer/PromoAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.retail.promo_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retail/promo_analyzer/PromoAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.promo_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
